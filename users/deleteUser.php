edaa32f6ad0cd9b1bff510634433fd59cf0ce96b
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-07-19 20:34:21 +0700

    delete user

--- users/deleteUser.php
+++ users/deleteUser.php
@@ -1,5 +1,18 @@
 <?php
 include 'connectUserDb.php';
 $conn = connectUserDb();
-$sql 
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+
+    $sql = "DELETE FROM users WHERE id = $id";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: viewUser.php");
+        exit();
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+} else {
+    echo "Invalid request.";
+}
 ?>
